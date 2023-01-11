2dc8ce55159ce3415290f2b2643cd326f9cad548
ivan <ivannovaa.24@gmail.com>
2023-01-11 18:37:38 +0700

    ini react-leaflet

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 const Home = () => {
-  return <main>asd</main>;
+  const position = [51.505, -0.09];
+  return (
+    <>
+      <main className="min-h-screen w-full">
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </main>
+    </>
+  );
 };
 
 export default Home;
