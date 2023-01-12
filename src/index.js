d98ce74d6c7f770b55317d62c95f23f9042fe540
Shironim <dimasseto.id@gmail.com>
2023-01-12 23:02:05 +0700

    update setup graphql apollo client

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// Apollo Provider
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo-client';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
